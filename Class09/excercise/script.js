e4b345c5f6a0c866781c7f646b7e13ffbbf49af5
lsim96 <leon.simonoskiii@gmail.com>
2025-02-05 12:17:28 +0100

    Class09

--- Class09/excercise/script.js
+++ Class09/excercise/script.js
@@ -13,30 +13,74 @@ let url = "https://restcountries.com/v2/alpha/mkd";
 //   });
 // }
 
-// console.log(getUrl());
+// console.log(getUrl(url));
 
-function getUrlPromise(url) {
-  return new Promise((resolve, reject) => {
-    $.ajax({
-      url: url,
-      success: function (response) {
-        resolve(response);
-      },
-      error: function (error) {
-        console.log("Rejected");
-        reject(error);
-      },
-    });
-  });
-}
+// function getUrlPromise(url) {
+//   return new Promise((resolve, reject) => {
+//     $.ajax({
+//       url: url,
+//       success: function (response) {
+//         resolve(response);
+//       },
+//       error: function (error) {
+//         console.log("Rejected");
+//         reject(error);
+//       },
+//     });
+//   });
+// }
 
-console.log(getUrlPromise(url));
+// console.log(getUrlPromise(url));
 
-// async function getUrlAsync(url) {
+// async function getUrlAsync() {
 //   let response = await fetch(url);
 //   let data = await response.json();
 //   console.log(data);
 //   return data;
 // }
 
-// console.log(getUrlAsync(url));
+// async function getNeighboursAsync() {
+//   let data = await getUrlAsync();
+//   console.log(data.borders);
+//   let html = "<ul>";
+//   html += `<li>${data.borders}</li>`;
+//   html += "<ul>";
+
+//   document.getElementById("content").innerHTML = html;
+// }
+
+// getNeighboursAsync();
+
+async function getCountryNeighboursAsync(countryCode) {
+  try {
+    let countryResponse = await fetch(
+      `https://restcountries.com/v2/alpha/${countryCode}`
+    );
+    let countryData = await countryResponse.json();
+
+    console.log("Country:");
+    console.log(countryData);
+
+    let neigbours = countryData.borders;
+
+    if (neigbours && neigbours.length > 0) {
+      console.log("Neigbours:");
+
+      for (let borderCode of neigbours) {
+        let neighboursResponse = await fetch(
+          `https://restcountries.com/v2/alpha/${borderCode}`
+        );
+        let neighbourData = await neighboursResponse.json();
+
+        console.log(neighbourData.name);
+        console.log(neighbourData);
+      }
+    } else {
+      console.log("No neighbouring countries.");
+    }
+  } catch (error) {
+    console.error("Error fetching country data:", error);
+  }
+}
+
+getCountryNeighboursAsync("SRB");
